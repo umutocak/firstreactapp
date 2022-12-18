168f183c2aa630c34fa533e646d59c9b71f82032
Umut Ocak <48753655+umutocak@users.noreply.github.com>
2022-12-18 22:31:14 +0300

    Added new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import User from './components/user';
 import './App.css';
 
 function App() {
+  const test = 22;
   return (
     <div className="App">
-      test
+      <h1>Merhaba React</h1>
+      <h3>{1+1} / {"umut".toUpperCase()}</h3>
+      <h3>{test}</h3>
+      <User />
     </div>
   );
 }
---
+++ src/components/user.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+            <input type="text" />
+            <button>BUTTON</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default User;
